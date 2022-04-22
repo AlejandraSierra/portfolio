6c1778f2cf4469dd960db3855d09b61c3ebbc0c2
Alejandra Sierra <asierra0321@gmail.com>
2022-04-23 01:34:40 +0200

    inclusión de metas

--- layout/navegacion.php
+++ layout/navegacion.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alejandra Sierra - <?php echo $nombrePagina ?></title>
+    <title>Alejandra Sierra | <?php echo $nombrePagina ?></title>
     <link rel="icon" type="image/png" href="../img/favicon.png">
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/style.css">
